e6ed50a0800fb29db48963cca0256ac81ca18cf7
eating-SUN <eatingsun@sjtu.edu.cn>
2025-06-06 21:37:36 +0800

    更新排序方式和footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -4,11 +4,11 @@
       <div class="row">
         <div class="col-md-6">
           <h5>研究团队</h5>
-          <p>上海交通大学bio2502课程小组<br>指导老师：吴茂英</p>
+          <p>上海交通大学bio2502课程第14小组<br>指导老师：吴茂英</p>
         </div>
         <div class="col-md-6">
-          <h5>联系方式</h5>
-          <p>email: contactinfo@sjtu.edu.cn<br>Tel: +86-010-12345678</p>
+          <h5>招生办联系方式</h5>
+          <p>email: zsb@sjtu.edu.cn<br>Tel: +86-021-34200000</p>
         </div>
       </div>
       <hr class="bg-light">
